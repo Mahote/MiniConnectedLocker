181506e81c260bdd1df8b7a3233ddc17fd97fe96
Thomas <thom.brnt@gmail.com>
2026-09-19 16:49:52 +0200

    playing with associatedtype

--- main.swift
+++ main.swift
@@ -1,21 +1,48 @@
 protocol SensorReadable {
-    func readValue() async -> Int
+    associatedtype Reading
+    var value: Reading {get set}
+    func readValue() async -> Reading
 }
 
-struct FakeHeartSensor: SensorReadable {
+class FakeHeartSensor: SensorReadable {
+    var value: Int = 0
     func readValue() async -> Int {
-        // Simulate reading a value from a sensor
-        try? await Task.sleep(nanoseconds: 500_000_000) // Simulate delay
-        return Int.random(in: 50...180)
+        try? await Task.sleep(nanoseconds: 500_000_000)
+        value = Int.random(in: 50...100)
+        return value
     }
 }
 
-func run() async {
-    let sensor = FakeHeartSensor()
+class TemperatureSensor: SensorReadable {
+    var value: Double = 0
+    func readValue() async -> Double {
+        try? await Task.sleep(nanoseconds: 500_000_000)
+        value = Double.random(in: -10.0...40.0)
+        return value
+    }
+}
+
+// class FakeHeartSensor: SensorReadable {
+// var value: Int = 0
+//     func readValue() async -> Int {
+//         // Simulate reading a value from a sensor
+//         try? await Task.sleep(nanoseconds: 500_000_000)
+//         self.setValue(newValue: Int.random(in: 50...100))
+//         return value
+//     }
+//     mutating func setValue(newValue: Reading){
+//         value = newValue
+//     }
+    
+// }
+
+func run<S: SensorReadable>(sensor: S) async {
     for _ in 1...5 {
         let bpm = await sensor.readValue()
         print("Heart Rate: \(bpm) BPM")
     }
 }
-
-await run()
+var fakeHeartSensor = FakeHeartSensor()
+var temperatureFakeSensor = TemperatureSensor()
+await run(sensor: fakeHeartSensor)
+await run(sensor: temperatureFakeSensor)
