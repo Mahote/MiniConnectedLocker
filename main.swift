0263b5704044d1950d03e7bdd24a8cb6ae553ddd
Thomas <thom.brnt@gmail.com>
2026-09-19 16:53:31 +0200

    added a describe method to have a prettier print

--- main.swift
+++ main.swift
@@ -2,6 +2,7 @@ protocol SensorReadable {
     associatedtype Reading
     var value: Reading {get set}
     func readValue() async -> Reading
+    func describe(value: Reading) -> String
 }
 
 class FakeHeartSensor: SensorReadable {
@@ -11,6 +12,10 @@ class FakeHeartSensor: SensorReadable {
         value = Int.random(in: 50...100)
         return value
     }
+
+    func describe(value: Int) -> String {
+        return "heartrate is \(value.description) bpm"
+    }
 }
 
 class TemperatureSensor: SensorReadable {
@@ -19,6 +24,10 @@ class TemperatureSensor: SensorReadable {
         try? await Task.sleep(nanoseconds: 500_000_000)
         value = Double.random(in: -10.0...40.0)
         return value
+    }
+
+    func describe(value: Double)  -> String {
+        return "Temperature is \(value.description) degrees"
     }
 }
 
@@ -39,7 +48,7 @@ class TemperatureSensor: SensorReadable {
 func run<S: SensorReadable>(sensor: S) async {
     for _ in 1...5 {
         let bpm = await sensor.readValue()
-        print("Heart Rate: \(bpm) BPM")
+        print(sensor.describe(value: bpm))
     }
 }
 var fakeHeartSensor = FakeHeartSensor()
